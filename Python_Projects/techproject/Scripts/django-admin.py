a3027ddcc8fdb6f7d848a09b7903af48dd2d26a8
Nicson Martinez <martineznicson@gmail.com>
2019-05-18 14:28:02 -0700

    Create Projects using the Django framework

---
+++ Python_Projects/techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\nicson~1\docume~1\github\th2832~1\python~1\techpr~1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
