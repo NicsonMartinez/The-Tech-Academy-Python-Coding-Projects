ba66ee21064981c54196210e80acab34daeea679
Nicson Martinez <martineznicson@gmail.com>
2019-05-28 13:59:14 -0700

    Update urls.py
    The a line to import 'include', and added a path which includes an 'include' method which passes in the urls.py file under 'products'.

--- Python_Projects/techproject/src/mainapp/products/views.py
+++ Python_Projects/techproject/src/mainapp/products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def admin_console(request):
+    # this is where we are  gonna store the data from  the datatbase
+    products = Product.objects.all()
+    return render(request, 'products/products_page.html', {'products': products})
